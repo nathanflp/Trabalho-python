3df622a58aee327cf9349df04ecff0239221a226
Nathan Feliphe <nathanfcrocha@gmail.com>
2022-09-26 20:51:22 -0300

    formatado certo
    funcional

--- lista1 ex1.py
+++ lista1 ex1.py
@@ -2,19 +2,19 @@ altura = float(input("\nInforme sua altura = "))
 print("\n1 - Homem | 2 - Mulher")
 sexo = int(input("informe seu sexo = "))
 
-while sexo !=1 or sexo !=2:
+while sexo !=1 and sexo !=2:
 
         print("\nDados inválidos,tente novamente!\n" "\n1 - Homem | 2 - Mulher")
         sexo = int(input("informe seu sexo = "))
 
-        if(sexo == 1):
+if(sexo == 1):
 
-                pesoIdeal = 72.7 * altura - 58
-                print(f"\nSeu peso ideal é de: {(round(pesoIdeal,1))} Kg(s)")
-                break
+        pesoIdeal = 72.7 * altura - 58
+        print(f"\nSeu peso ideal é de: {(round(pesoIdeal,1))} Kg(s)")
+        
 
-        elif(sexo == 2):
+elif(sexo == 2):
 
-                pesoIdeal = 62.1 * altura - 44.7
-                print(f"\nSeu peso ideal é de: {(round(pesoIdeal,1))} Kg(s)")
-                break
+        pesoIdeal = 62.1 * altura - 44.7
+        print(f"\nSeu peso ideal é de: {(round(pesoIdeal,1))} Kg(s)")
+        
