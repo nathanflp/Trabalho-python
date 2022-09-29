395c071fb4294746a7c39a99c5783b96f9862e7f
Nathan Feliphe <101686889+nathanflp@users.noreply.github.com>
2022-09-29 12:59:06 -0300

    comentarios
    adicionando comentários

--- lista1 ex1.py
+++ lista1 ex1.py
@@ -2,17 +2,17 @@ altura = float(input("\nInforme sua altura = "))
 print("\n1 - Homem | 2 - Mulher")
 sexo = int(input("informe seu sexo = "))
 
-while sexo !=1 and sexo !=2:
+while sexo !=1 and sexo !=2: #loop caso sexo seja diferente do solicitado
 
         print("\nDados inválidos,tente novamente!\n" "\n1 - Homem | 2 - Mulher")
         sexo = int(input("informe seu sexo = "))
 
-if(sexo == 1):
+if(sexo == 1): #Caso 1 Homem
 
         pesoIdeal = 72.7 * altura - 58
         print(f"\nSeu peso ideal é de: {(round(pesoIdeal,1))} Kg(s)")
 
-elif(sexo == 2):
+elif(sexo == 2): #Caso 2 Mulher
         
         pesoIdeal = 62.1 * altura - 44.7
         print(f"\nSeu peso ideal é de: {(round(pesoIdeal,1))} Kg(s)")
